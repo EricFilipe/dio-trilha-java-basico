dab2e0f951bc074fa034d0df137c5b9f4e0eefda
Eric <ericfilipe125@gmail.com>
2025-10-03 18:12:35 -0300

    Deixando os parametros para passar no args

--- Sudoku_Game2/SudokuGame2/src/Main.java
+++ Sudoku_Game2/SudokuGame2/src/Main.java
@@ -8,5 +8,13 @@ public class Main {
         Menu menu = new Menu();
 
         menu.play(args);
+
+        //Parametros para adicionar no args/running do projeto
+        /*9 5 8 empty empty empty empty 2 empty empty empty empty
+        2 5 6 empty 4 empty empty empty 6 empty empty empty 5 1 7
+        6 empty empty 3 7 8 empty empty empty 7 8 4 empty empty empty
+        9 3 2 empty empty empty 4 2 9 empty empty 8 4 9 2 empty empty
+        empty 1 empty empty empty 6 empty 5 8 1 empty empty empty empty
+        1 empty empty empty empty 7 6 3*/
     }
 }
