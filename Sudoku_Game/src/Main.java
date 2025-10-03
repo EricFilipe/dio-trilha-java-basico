67599d77753a6ad32baf4ad0bf81ffd8d2e9da19
Eric <ericfilipe125@gmail.com>
2025-10-03 18:05:17 -0300

    Deixando os numeros para adicionar no args

--- Sudoku_Game/src/Main.java
+++ Sudoku_Game/src/Main.java
@@ -9,5 +9,12 @@ public class Main {
         Menu menu = new Menu();
 
         menu.play(args);
+
+        //Numeros para adicionar no args/running do projeto
+        /*1 1 9 1 2 5 1 3 8 1 8 2 2 4 2 2 5 5 2 6 6 2 8 4 3
+        3 6 3 7 5 3 8 1 3 9 7 4 1 6 4 4 3 4 5 7 4 6 8 5 1 7
+        5 2 8 5 3 4 5 7 9 5 8 3 5 9 2 6 4 4 6 5 2 6 6 9 6 9
+        8 7 1 4 7 2 9 7 3 2 7 7 1 8 2 6 8 4 5 8 5 8 8 6 1 9
+        2 1 9 7 7 9 8 6 9 9 3*/
     }
 }
